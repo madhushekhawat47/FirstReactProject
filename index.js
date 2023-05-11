0587a2637c2945e8957cdb2509435537db1bdd3f
madhushekhawat47 <94750729+madhushekhawat47@users.noreply.github.com>
2023-05-11 22:27:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+import "./index.css";
+
+
+ReactDOM.render(
+    <App/>,
+document.getElementById("root")
+);
+// //var React= require('react');
+// import React from 'react';
+// //var ReactDOM = require('react-dom');
+// import ReactDOM  from 'react-dom';
+
+// /*ReactDOM.render('kya dikana hai','kha dikana hai','callback func');*/
+// ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'))
